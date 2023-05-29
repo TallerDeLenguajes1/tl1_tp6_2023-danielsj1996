15eefbf443aaa48a2e94fa07e67d40b2244ce4ad
“Daniel <danieljuarez1996@gmail.com>
2023-05-29 00:26:15 -0300

    Ejercicio 4 tp6

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,12 +1,15 @@
 ﻿internal class Program
 {
+    
     private static void Main(string[] args)
     {
+
         Console.WriteLine("Bienvenido/a");
 
         int numero = 0;
         bool funciona = false;
         string? numString = "";
+
 
         int OrdenInverso(int numero)
         {
@@ -43,6 +46,62 @@
                 }
             }
         }
+
+        Console.WriteLine("Cadenas");
+        Console.WriteLine("Ingrese una Cadena de Caracteres: ");
+        string? Cadena1 = Console.ReadLine();
+
+        int longitud = Cadena1.Length;
+        Console.WriteLine("La longitud de la 1ra Cadena es: \n" + longitud);
+
+        Console.WriteLine("Ingrese una 2da Cadena de Caracteres: ");
+        string? Cadena2 = Console.ReadLine();
+
+        string Cadena3 = Cadena1 + Cadena2;
+        Console.WriteLine("Las 2 cadenas concatenadas dan como resultado: " + Cadena3);
+
+        string subCadena = Cadena3.Substring(5, 5);
+        Console.WriteLine("La subcadena es: \n" + subCadena);
+
+
+
+        Console.Write("\n");
+        Console.Write("\n");
+        Console.Write("Recorrido de la Cadena: \n");
+        foreach (char caracter in Cadena3)
+        {
+            Console.WriteLine(caracter);
+        }
+
+
+
+        Console.WriteLine("Ingrese la palabra a buscar: \n");
+        string? palabra = Console.ReadLine();
+        bool busqueda = Cadena3.Contains(palabra, System.StringComparison.CurrentCultureIgnoreCase);
+
+        Console.WriteLine($"La cadena contiene la palabra buscada? {busqueda}");
+
+        Console.Write("\n");
+        Console.WriteLine("Cadena en Mayusculas: \n" + Cadena3.ToUpper());
+        Console.Write("\n");
+
+        Console.WriteLine("Cadena en Minusculas: \n" + Cadena3.ToLower());
+
+        Console.Write("\n");
+        Console.WriteLine("Ingresa una cadena separada por punto y coma (;): \n");
+        string? cadena = Console.ReadLine();
+        char delimitador = ';';
+        string[] valores = cadena.Split(delimitador, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine("Los resultados son:");
+
+        foreach (string resultado in valores)
+        {
+            Console.WriteLine(resultado);
+        }
+
+
+
+
     }
 
 }
